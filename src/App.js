01878eee006cc2d840aec547691f1f9fc4d004d0
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-09-19 11:26:26 -0500

    Create App main component

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+import Header from './components/core/Header';
+import Footer from './components/core/Footer';
+
+import Home from './components/home/Home';
+import SearchPage from './components/search/SearchPage';
+import Publish from './components/publish/Publish';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/search" component={SearchPage} />
+        <Route exact path="/publish" component={Publish} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
