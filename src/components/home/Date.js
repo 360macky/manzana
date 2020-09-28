7b411166a2c10f3c7d09cd1c85ba6389f7e4a8ab
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-09-28 15:10:32 -0500

    Create Date component

---
+++ src/components/home/Date.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../styles/home/Date.css';
+import Search from '../search/Search';
+
+function Date() {
+  return (
+    <div className="date">
+      <Search />
+    </div>
+  );
+}
+
+export default Date;
